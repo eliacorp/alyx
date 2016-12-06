b45ede6556e66ab9fed581c59c3cf3c455bd3c3a
eliafornari <fornari.elia@gmail.com>
2016-12-06 01:53:20 -0800

    removed RU and CH from taxable countries

--- server/main.js
+++ server/main.js
@@ -345,10 +345,10 @@ function setToHappen(d){
     }
 
 
-    var Europe = ['AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE',
+    var Europe = ['AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CY', 'CZ', 'DE',
         'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR',
         'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PL',
-        'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA'];
+        'PT', 'RO', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA'];
 
 
     function cartToOrder(req, res, data){
